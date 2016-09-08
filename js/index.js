499f284f18e6517724ee024563885a6ec73bf372
Ivan shyyka <i.shyyka@ukr.net>
2016-09-08 15:13:02 +0000

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
         var $dropdown = $("#dropdown");
         
-        
-        
         $(window).scroll(function(){
             if ( $(this).scrollTop() > 700 && $dropdown.hasClass("default") ){
                 $dropdown.removeClass("default").addClass("fixed");
@@ -10,4 +8,46 @@ $(document).ready(function(){
                 $dropdown.removeClass("fixed").addClass("default");
             }
         });//scroll
+        
+        
+        
+        //плавний скрол меню
+        
+        $("#menu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+        });
+        
+       //плавний скрол випад. меню
+         $("#dropdown").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+        });
+    //collaps trees
+         $("#hide-t").click(function(){
+         $("div.collaps-t").hide(1000);
+         });
+         $("#show-t").click(function(){
+         $("div.collaps-t").show(1000);
+         });
+    //collaps flowers
+         $("#hide-f").click(function(){
+         $("div.collaps-f").hide(1000);
+         });
+         $("#show-f").click(function(){
+         $("div.collaps-f").show(1000);
+         });
+        
     });
